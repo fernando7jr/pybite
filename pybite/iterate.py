67bb0413ec1b34a7c0488cbf782992e485ce1a26
fernando.settijunior <fernando.settijunior@gmail.com>
2019-09-05 16:46:31 -0300

    Added new options to iterate_by.
    Added header persting for iterate_by.

--- pybite/iterate.py
+++ pybite/iterate.py
@@ -5,30 +5,38 @@ def __map_same(x):
     return x
 
 
-def __gen(iterable: iter, count: int, _map=None):
-    buffer = []
-    _len = 0
+def __gen(iterable: iter, count: int, _map=None, header=None):
+    def _make_chunk():
+        if header is not None:
+            return [header]
+        return []
+    
+    def _chunk_len():
+        l = len(buffer)
+        return l - 1 if header is not None else l
+
+    buffer = _make_chunk()
     if _map is None:
         _map = __map_same
     for item in iterable:
+        _len = _chunk_len()
         if _len == count:
             yield buffer
-            _len = 1
-            buffer = [_map(item)]
-        else:
-            buffer.append(_map(item))
-            _len += 1
-    if _len > 0:
+            buffer = _make_chunk()
+        buffer.append(_map(item))
+    if _chunk_len() > 0:
         yield buffer
 
 
-def iterate_by(iterable: iter, chunk_size: int, map=None) -> iter:
+def iterate_by(iterable: iter, chunk_size: int, map=None,
+persist_header=False, header=None) -> iter:
     """
     Return a iterator of chunks for the iterable.
 
     A new iterable is formed from the passed data and divided in chunks.
     Each chunk is generated on-demand and its size will be chunk_size or less
     in case there is not enough data to fill it.
+    In case persist_header is True, the size of each chunk will be increased by 1.
 
     Parameters
     ----------
@@ -38,6 +46,14 @@ def iterate_by(iterable: iter, chunk_size: int, map=None) -> iter:
         The size of each chunk
     map : callable optional, defaults None
         A map function for transform the data before dividing in chunks
+    persist_header : bool optional, defaults False
+        Whether to persist a header on each chunk or not. 
+        If persist_header is True and header is None, 
+        the firt line will be read as the actual header.
+    header : str optional, defaults None
+        A header to be written at the start of each file.
+        If persist_header is True and header is None, 
+        the firt line will be read as the actual header.
     Returns
     -------
     iter
@@ -49,4 +65,8 @@ def iterate_by(iterable: iter, chunk_size: int, map=None) -> iter:
     >>> iterate_by([1, 2, 3, 4, 5], 2, map=lambda x: x * 2)
     iter([2, 4], [6, 8], [10])
     """
-    return __gen(iterable, chunk_size, _map=map)
+    if persist_header is True:
+        if header is None:
+            iterable = iter(iterable)
+            header = iterable.__next__()
+    return __gen(iterable, chunk_size, _map=map, header=header)
--- test/test_iterate.py
+++ test/test_iterate.py
@@ -8,8 +8,26 @@ test_data = [
 
 
 def test_iterate_by():
-    iter_data = list(iterate_by(test_data, 4))
+    result = list(iterate_by(test_data, 4))
     for i in range(0, 4):
         test_data_chunk = "".join(test_data[i*4:(i+1)*4])
-        iter_data_chunk = "".join(iter_data[i])
-        assert test_data_chunk == iter_data_chunk
+        result_chunk = "".join(result[i])
+        assert test_data_chunk == result_chunk
+
+
+def test_iterate_by_with_header():
+    header = "letter"
+    result = list(iterate_by(test_data, 4, 
+    persist_header=True, header=header))
+    for i in range(0, 4):
+        test_data_chunk = "".join([header] + test_data[i*4:(i+1)*4])
+        result_chunk = "".join(result[i])
+        assert test_data_chunk == result_chunk
+
+    result = list(iterate_by([header] + test_data, 4, 
+    persist_header=True))
+    for i in range(0, 4):
+        test_data_chunk = "".join([header] + test_data[i*4:(i+1)*4])
+        result_chunk = "".join(result[i])
+        assert test_data_chunk == result_chunk
+
